2c287abca608e66d8f3938d9c25fbcc37b89f0d4
prembhimavat <89598604+prembhimavat@users.noreply.github.com>
2022-02-05 18:47:23 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const https = require("https");
+const bodyparser = require("body-parser");
+
+const app = express();
+
+app.use(bodyparser.urlencoded({extended: true}));
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req,res){
+    const query = req.body.cityName;
+    const appid = "ab5af4a821720ab4c74f82b82f6f044e";
+    const unit = req.body.units;
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+appid+"&units="+unit;
+    https.get(url,function(response){
+        console.log(response.statusCode);
+
+        response.on("data",function(data){
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const weathertype = weatherData.weather[0].main;
+            const icon = weatherData.weather[0].icon;
+            const minTemp = weatherData.main.temp_min;
+            const maxTemp = weatherData.main.temp_max;
+            const windSpeed = weatherData.wind.speed;
+            const humid = weatherData.main.humidity;
+
+            var imageURL = "http://openweathermap.org/img/w/" + icon + ".png";
+            res.setHeader("Content-Type", "text/html");
+            res.write("<img src=" + imageURL + ">");
+            res.write("<h3>The weather currenly is <u>" +  weathertype + " </u></h3>");
+            res.write("<h3>Current temprature in "+query+" is <u>" + temp + "</u></h3>");
+            res.write("<h3>Minimum temprature recorded : <u>"+minTemp+"</u></h3>");
+            res.write("<h3>Maximum temprature recorded : <u>" + maxTemp +"</u></h3>");
+            res.write("<h3>Wind speed : <u>" + windSpeed + "</u></h3>");
+            res.write("<h3>Humidity(%) : <u>" + humid + "</u></h3");
+            res.send();
+        });
+    });
+});
+
+var port = process.env.PORT || 3000;
+
+app.listen(port,function(){
+    console.log("Server is running on port 3100");
+});
